3f1026db4a6ed77d2d7d90a470b0617b5e639379
chernandezba <chernandezba@hotmail.com>
2021-03-01 19:31:11 +0100

    mc

---
+++ extras/media/spectrum/tbblue/sprites/ZXSpectrumNextTests/BigSprite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zesarux --noconfigfile --quickexit --nowelcomemessage --zoom 1 --machine tbblue --realvideo --enabletimexvideo --tbblue-fast-boot-mode --sna-no-change-machine `pwd`/SpritBig.sna
